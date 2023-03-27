bf7f73d454d7669abbf21a6efb919c360ade0bc3
Ande Samuel Babajide <65017581+andesamuelb@users.noreply.github.com>
2023-03-27 11:19:29 +0100

    Task Fixed

--- get_functions3.c
+++ get_functions3.c
@@ -9,7 +9,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed. Samuel
+ * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
